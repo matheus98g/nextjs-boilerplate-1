a35d65a414ad3f15f03ec57a938c63872f1a5bda
matheus98g <gollmannmatheus@gmail.com>
2025-10-15 20:44:23 -0300

    init

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,6 +1,4 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-
 
 export default function Dashboard() {
     return (
@@ -9,13 +7,8 @@ export default function Dashboard() {
                 className="w-full max-w-md mx-auto mt-10 p-4"
             >
                 <CardHeader>
-                    <CardTitle>Controle de Boletos</CardTitle>
+                    <CardTitle>Dashboard</CardTitle>
                 </CardHeader>
-                <CardContent
-                    className="flex justify-center"
-                >
-                    <Button>Adicionar Boleto</Button>
-                </CardContent>
             </Card>
         </>
     )
