9db05d8b4328dc1d1e246078029013bf24b9e54a
matheus98g <gollmannmatheus@gmail.com>
2025-10-15 21:00:28 -0300

    clerk auth basic config ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,35 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
+import { SignInButton, SignOutButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-      <Card
-        className="w-full max-w-md mx-auto mt-10 p-4"
-      >
-        <CardContent
-          className="flex justify-center"
-        >
-          <Link href="/dashboard">
-            <Button>Ir para o Dashboard</Button>
-          </Link>
+      <Card className="w-full max-w-md mx-auto mt-10 p-4">
+        <CardContent className="flex justify-center">
+          <SignedOut>
+            <div className="flex justify-center gap-2">
+              <SignInButton>
+                <Button>Entrar</Button>
+              </SignInButton>
+              <SignUpButton>
+                <Button>Cadastrar</Button>
+              </SignUpButton>
+            </div>
+          </SignedOut>
+          <SignedIn>
+            <div className="flex flex-col items-center gap-4">
+              <span className="text-lg font-medium">Você já está autenticado!</span>
+              <div className="flex items-center gap-3">
+                <UserButton />
+                <SignOutButton>
+                  <Button>Sair</Button>
+                </SignOutButton>
+              </div>
+            </div>
+          </SignedIn>
         </CardContent>
       </Card>
     </>
-  )
+  );
 }
